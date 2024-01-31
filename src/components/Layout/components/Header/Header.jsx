43281a2d6c95c845341e215c12d3bebb4d431e10
Oleksii Tuhai <alextugai73@gmail.com>
2024-01-31 22:12:12 +0100

    update Header component

--- src/components/Layout/components/Header/Header.jsx
+++ src/components/Layout/components/Header/Header.jsx
@@ -3,29 +3,39 @@ import { Logo } from '../../../commons/';
 import { NavLink } from 'react-router-dom';
 import stylesDark from './styles/dark.module.css';
 import stylesLight from './styles/light.module.css';
+import { loadFromLocalStorage } from '../../../../utils';
 
 export const Header = () => {
-   const userId = useParams();
+   const userId = loadFromLocalStorage('userId');
    const [isDarkTheme, setIsDarkTheme] = useState(false);
    const styles = isDarkTheme ? stylesDark : stylesLight;
+
+   const onChangeHandler = () => {
+      setIsDarkTheme(!isDarkTheme);
+   }
 
    return (
       <header className={styles.header}>
          <div className="container">
             <nav className={styles.nav}>
-               <Logo />
 
-               <ul>
-                  <li><NavLink to='/cards'>Cards</NavLink></li>
-                  <li><NavLink to='/transactions'>Transactions</NavLink></li>
-                  <li><NavLink to='/profile'>Profile</NavLink></li>
-               </ul>
-               
-               <button onClick={() => setIsDarkTheme(!isDarkTheme)}>
-                  Toggle Theme
-               </button>
+               <div className="leftPart">
+                  <Logo />
+               </div>
+
+               <div className={styles.rightPart}>
+
+                  <input onChange={onChangeHandler} type="checkbox" className={styles.chechBox} />
+
+                  <ul className={styles.menu}>
+                     <li className={styles.menu__item}><NavLink to={userId ? `/${userId}/cards` : '/login'}>Cards</NavLink></li>
+                     <li className={styles.menu__item}><NavLink to={userId ? `/${userId}/transactions` : '/login'}>Transactions</NavLink></li>
+                     <li className={styles.menu__item}><NavLink to={userId ? `/${userId}/profile` : '/login'}>Profile</NavLink></li>
+                  </ul>
+               </div>
+
             </nav>
-         </div>
-      </header>
+         </div >
+      </header >
    );
 };
