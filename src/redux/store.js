a4700c47d55d5e592ae0ecff3441bd25be8d07df
Oleksii Tuhai <alextugai73@gmail.com>
2024-01-31 20:14:33 +0100

    update store api

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,16 +3,13 @@ import { cardsApi } from "./cardsApi";
 import { usersApi } from "./usersApi";
 import { exchangeRateApi } from "./exchangeRateApi";
 
-// Створення центрального зберігання (store) з використанням конфігурації Redux Toolkit
 export const store = configureStore({
-   // визначення редукторів для обробки стану, пов'язаного з API
    reducer: {
       [cardsApi.reducerPath]: cardsApi.reducer,
       [usersApi.reducerPath]: usersApi.reducer,
       [exchangeRateApi.reducerPath]: exchangeRateApi.reducer,
    },
 
-   // Додаємо middleware для обробки асинхронних операцій, пов'язаних з API
    middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware().concat(cardsApi.middleware, usersApi.middleware, exchangeRateApi.middleware),
 });
