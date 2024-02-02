a453a8732b056426f257ddbbca36390986bb52a3
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-02 23:01:28 +0100

    add footer + styles

--- src/components/Layout/components/Footer/Footer.jsx
+++ src/components/Layout/components/Footer/Footer.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
+import styles from './footer.module.css';
+import { Img, Logo } from '../../../commons';
 
 export const Footer = () => {
    return (
-      <footer className='footer'>
-         <div className="container">
-            
+      <footer className={styles.footer}>
+         <div className='container'>
+            <div className={styles.footerContent}>
+               <div className={styles.contactInfo}>
+                  <div className={styles.email}>
+                     <span>Email:</span>
+                     <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+                     <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+                     <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+                     <a href="mailto:info@yourcompany.com">info@yourcompany.com</a>
+                  </div>
+               </div>
+               <div className={styles.contactInfo}>
+                  <div className={styles.phoneNumber}>
+                     <span>Phone:</span>
+                     <a href="tel:+123456789">+1 (234) 567-89</a>
+                     <a href="tel:+123456789">+1 (234) 567-89</a>
+                     <a href="tel:+123456789">+1 (234) 567-89</a>
+                     <a href="tel:+123456789">+1 (234) 567-89</a>
+                  </div>
+               </div>
+               <div className={styles.socialIcons}>
+                  <a href='https://www.facebook.com/'><Img folder='icon' img='facebook.png' /></a>
+                  <a href='https://www.instagram.com/'><Img folder='icon' img='instagram.png' /></a>
+                  <a href='https://www.messenger.com/'><Img folder='icon' img='messenger.png' /></a>
+               </div>
+            </div>
+            <div className={styles.copyRight}>
+               &copy; 2024 Some Company. All rights reserved.
+            </div>
          </div>
       </footer>
    );
