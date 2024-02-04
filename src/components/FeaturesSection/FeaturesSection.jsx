93d00218840f04e21245ab49b76e8f79eea6898b
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-04 17:42:44 +0100

    update

--- src/components/FeaturesSection/FeaturesSection.jsx
+++ src/components/FeaturesSection/FeaturesSection.jsx
@@ -1,25 +1,33 @@
 import React from 'react';
 import styles from './FeaturesSection.module.css';
+import { Img } from '../commons';
 
 export const FeaturesSection = () => {
    return (
       <section className={styles.featuresSection}>
-         <div className={styles.feature}>
-            <div className={styles.featureIcon}>Image</div>
-            <h3>Convenient Service</h3>
-            <p>Our services are always at your convenience.</p>
+         <div className={styles.featureRow}>
+            <div className={styles.feature}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='service.png' />
+               </div>
+               <h3>Convenient Service</h3>
+            </div>
+
+            <div className={styles.feature}>
+               <h3>Data Security</h3>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='security.png' />
+               </div>
+            </div>
          </div>
 
-         <div className={styles.feature}>
-            <div className={styles.featureIcon}>Image</div>
-            <h3>Data Security</h3>
-            <p>We guarantee the confidentiality and security of your financial data.</p>
-         </div>
-
-         <div className={styles.feature}>
-            <div className={styles.featureIcon}>Image</div>
-            <h3>Flexibility</h3>
-            <p>Choose from various financial products and services to meet your needs.</p>
+         <div className={styles.featureRow}>
+            <div className={styles.feature}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='flexibility.png' />
+               </div>
+               <h3>Flexibility</h3>
+            </div>
          </div>
       </section>
    );
--- src/pages/HomePage/HomePage.jsx
+++ src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
-import { CardCreationBlock, ExchangeRate, HeroSection } from '../../components';
+import { CardCreationBlock, ExchangeRate, FeaturesSection, HeroSection } from '../../components';
 import styles from './homePage.module.css';
 
 export const HomePage = () => {
    return (
       <div className='homePage'>
          <HeroSection />
+         <div className="container">
+            <FeaturesSection />
+         </div>
          <div className="container">
             <CardCreationBlock />
          </div>
