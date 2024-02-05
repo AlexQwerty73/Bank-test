44c36cef6edf41e351ffe405ca7fcdfc6ff1ec28
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-05 17:52:36 +0100

    update/fix FeaturesSection

--- src/components/FeaturesSection/FeaturesSection.jsx
+++ src/components/FeaturesSection/FeaturesSection.jsx
@@ -1,4 +1,3 @@
-// FeaturesSection.jsx
 import React from 'react';
 import styles from './FeaturesSection.module.css';
 import { Img } from '../commons';
@@ -6,36 +5,42 @@ import { Img } from '../commons';
 export const FeaturesSection = () => {
    return (
       <section className={styles.featuresSection}>
-         <div className={styles.featureRow}>
-            <div className={styles.feature}>
-               <div className={styles.featureContent}>
-                  <div className={styles.featureIcon}>
-                     <Img folder='featuresSection' img='service.png' />
-                  </div>
-                  <h3>Convenient Service</h3>
+         <div className={styles.feature}>
+            <div className={styles.featureContent}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='service.png' />
                </div>
-            </div>
-
-            <div className={styles.feature}>
-               <div className={styles.featureContent}>
-                  <div className={styles.featureIcon}>
-                     <Img folder='featuresSection' img='security.png' />
-                  </div>
-                  <h3>Data Security</h3>
-               </div>
+               <h3 className={styles.h3}>Convenient Service</h3>
             </div>
          </div>
 
-         <div className={styles.featureRow}>
-            <div className={styles.feature}>
-               <div className={styles.featureContent}>
-                  <div className={styles.featureIcon}>
-                     <Img folder='featuresSection' img='flexibility.png' />
-                  </div>
-                  <h3>Flexibility</h3>
+         <div className={styles.feature}>
+            <div className={styles.featureContent}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='security.png' />
                </div>
+               <h3 className={styles.h3}>Data Security</h3>
             </div>
          </div>
+
+         <div className={styles.feature}>
+            <div className={styles.featureContent}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='flexibility.png' />
+               </div>
+               <h3 className={styles.h3}>Flexibility</h3>
+            </div>
+         </div>
+
+         <div className={styles.feature}>
+            <div className={styles.featureContent}>
+               <div className={styles.featureIcon}>
+                  <Img folder='featuresSection' img='support.png' />
+               </div>
+               <h3 className={styles.h3}>24/7 Customer Support</h3>
+            </div>
+         </div>
+
       </section>
    );
 };
