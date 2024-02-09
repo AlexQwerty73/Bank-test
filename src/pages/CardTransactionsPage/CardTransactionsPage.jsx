8e9ab8965ee86d65077577ec41628d771005b902
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-09 18:12:38 +0100

    update TransactionsPage/styles

--- src/pages/CardTransactionsPage/CardTransactionsPage.jsx
+++ src/pages/CardTransactionsPage/CardTransactionsPage.jsx
@@ -4,6 +4,17 @@ import { useGetCardByNumberQuery } from '../../redux';
 import styles from './CardTransactionsPage.module.css';
 import { SelectedTransaction } from '../../components';
 
+const formatDate = (dateString) => {
+   const date = new Date(dateString);
+   const hours = ('0' + date.getHours()).slice(-2);
+   const minutes = ('0' + date.getMinutes()).slice(-2);
+   const day = ('0' + date.getDate()).slice(-2);
+   const month = ('0' + (date.getMonth() + 1)).slice(-2);
+   const year = date.getFullYear();
+
+   return `${hours}:${minutes} ${day}.${month}.${year}`;
+};
+
 export const CardTransactionsPage = () => {
    const { cardNumber } = useParams();
    const { data: card = [], isLoading } = useGetCardByNumberQuery(cardNumber);
@@ -28,13 +39,13 @@ export const CardTransactionsPage = () => {
          <ul>
             {card[0]?.history?.map(transaction => (
                <li key={transaction.date} onClick={() => handleTransactionClick(transaction)} className={styles.transaction}>
-                  <div>Date: {transaction.date}</div>
+                  <div>Date: {formatDate(transaction.date)}</div>
                   <div>Amount: {transaction.amount}</div>
                </li>
             ))}
          </ul>
-         
-         {selectedTransaction && <SelectedTransaction card={card} selectedTransaction={selectedTransaction} handleCloseTransaction={handleCloseTransaction}/>}
+
+         {selectedTransaction && <SelectedTransaction card={card} selectedTransaction={selectedTransaction} handleCloseTransaction={handleCloseTransaction} />}
       </div>
    );
 };
--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-import { useGetCardsByUserIdQuery, useGetUsersQuery } from '../../redux';
-import { loadFromLocalStorage } from '../../utils';
+import { Link, useParams } from 'react-router-dom';
+import { useGetCardsByUserIdQuery } from '../../redux';
+import { convertToNumberCartFormat } from '../../utils';
+import styles from './TransactionsPage.module.css';
 
 export const TransactionsPage = () => {
-   const userId = loadFromLocalStorage('userId');
+   const { userId } = useParams();
    const { data: cards = [], isLoading } = useGetCardsByUserIdQuery(userId);
 
    return (
-      <div>
-         <h1>Transactions Page</h1>
-         <ul>
-            {cards.map(card => (
-               <li key={card?.id}>
-                  <Link to={`${card.number}`}>
-                     Card Number: {card.number}
-                  </Link>
-               </li>
-            ))}
-         </ul>
+      <div className={styles.transactionsPage}>
+         <div className='container'>
+            <ul className={styles.transactionsList}>
+               {cards.map(card => (
+                  <li key={card?.id} className={styles.transactionItem}>
+                     <Link to={`${card.number}`} className={styles.transactionLink}>
+                        <div className={styles.transactionDetails}>
+                           <p className={styles.cardNumber}>
+                              Card Number: {convertToNumberCartFormat(card.number)}
+                           </p>
+                           <p className={styles.balance}>
+                              Balance: {card.balance} {card.currency}
+                           </p>
+                        </div>
+                     </Link>
+                  </li>
+               ))}
+            </ul>
+         </div>
       </div>
    );
 };
