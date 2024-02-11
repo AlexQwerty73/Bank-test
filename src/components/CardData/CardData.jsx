b6a555b8d003632e4c752dd759b28bc84a9db071
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-11 13:19:18 +0100

    update carddata

--- src/components/CardData/CardData.jsx
+++ src/components/CardData/CardData.jsx
@@ -1,90 +1,60 @@
 import React, { useState } from 'react';
 import styles from './cardData.module.css';
-import { Card, EditBtn, Img } from '../commons';
 import { convertToNumberCartFormat } from '../../utils';
 import { useUpdateCardMutation } from '../../redux';
+import { Card, EditBtn, Img } from '../commons';
 
 export const CardData = ({ card }) => {
    const { number, expiryDate, cvv, balance, type, currency } = card;
    const [updateCard] = useUpdateCardMutation();
-
    const [isCvvEdit, setIsCvvEdit] = useState(false);
    const [cvvEdit, setCvvEdit] = useState(cvv);
 
-   const onClickHandler = (item, newData) => {
-      console.log('click!');
+   const onClickHandler = () => {
       setIsCvvEdit(!isCvvEdit);
-
-      const newCardData = {
-         ...card,
-         [item]: newData
-      }
-
-      if (cvvEdit.length !== 3 && !/^\d{3}$/.test(newData)) {
+      if (cvvEdit.length !== 3 && !/^\d{3}$/.test(cvvEdit)) {
          setCvvEdit(cvv);
          console.log('CVV should have exactly 3 numeric digits');
-      } else if (cvv !== cvvEdit) {
-         updateCard(newCardData).unwrap();
-         console.log(newCardData);
+         return;
       }
-   }
+      const newCardData = { ...card, cvv: cvvEdit };
+      if (cvv !== cvvEdit) updateCard(newCardData).unwrap();
+   };
+
+   const renderData = (label, value) => (
+      <div className={styles.data__item}>
+         <div className={styles.item__data}>
+            <h3 className={styles.h3}>{label}</h3>
+            <h3 className={styles.h3}>{value}</h3>
+         </div>
+      </div>
+   );
+
+   const renderCvvData = () => (
+      <div className={styles.data__item}>
+         <div className={styles.item__data}>
+            <h3 className={styles.h3}>CVV:</h3>
+            {!isCvvEdit ? (
+               <h3 className={styles.h3}>{cvv}</h3>
+            ) : (
+               <input type="number" value={cvvEdit} onChange={e => setCvvEdit(e.target.value)} maxLength={3} />
+            )}
+         </div>
+         <EditBtn isEdit={isCvvEdit} onClick={onClickHandler} />
+      </div>
+   );
 
    return (
       <div className={styles.cardData}>
          <div className={styles.leftPart}>
             <h1 className={styles.h1}>Card Data:</h1>
             <div className={styles.data}>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>Number:</h3>
-                     <h3 className={styles.h3}>{convertToNumberCartFormat(number)}</h3>
-                  </div>
-               </div>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>Expiry date:</h3>
-                     <h3 className={styles.h3}>{expiryDate.split('-').reverse().join(' ')}</h3>
-                  </div>
-               </div>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>CVV:</h3>
-                     {
-                        !isCvvEdit
-                           ? <h3 className={styles.h3}>{cvv}</h3>
-                           : <input type='number' value={cvvEdit} onChange={e => setCvvEdit(e.target.value)} maxLength={3} />
-                     }
-
-                  </div>
-                  
-                  <EditBtn isEdit={isCvvEdit} onClick={() => onClickHandler('cvv', cvvEdit)} />
-
-               </div>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>Balance:</h3>
-                     <h3 className={styles.h3}>{balance}</h3>
-                  </div>
-               </div>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>Currency:</h3>
-                     <h3 className={styles.h3}>{currency}</h3>
-                  </div>
-               </div>
-
-               <div className={styles.data__item}>
-                  <div className={styles.item__data}>
-                     <h3 className={styles.h3}>Type:</h3>
-                     <h3 className={styles.h3}>{type}</h3>
-                  </div>
-               </div>
-
+               {renderData('Number:', convertToNumberCartFormat(number))}
+               {renderData('Expiry date:', expiryDate.split('-').reverse().join(' '))}
+               {renderCvvData()}
+               {renderData('Balance:', balance)}
+               {renderData('Currency:', currency)}
+               {renderData('Type:', type)}
             </div>
          </div>
          <div className={styles.rightPart}>
