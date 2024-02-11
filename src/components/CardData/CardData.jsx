7b4a153367572ef75e9bf58ce9d969314fede448
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-11 13:32:58 +0100

    update CardData styles

--- src/components/CardData/CardData.jsx
+++ src/components/CardData/CardData.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import styles from './cardData.module.css';
 import { convertToNumberCartFormat } from '../../utils';
 import { useUpdateCardMutation } from '../../redux';
-import { Card, EditBtn, Img } from '../commons';
+import { Card, EditBtn } from '../commons';
 
 export const CardData = ({ card }) => {
    const { number, expiryDate, cvv, balance, type, currency } = card;
@@ -12,35 +12,27 @@ export const CardData = ({ card }) => {
 
    const onClickHandler = () => {
       setIsCvvEdit(!isCvvEdit);
-      if (cvvEdit.length !== 3 && !/^\d{3}$/.test(cvvEdit)) {
+      const isValidCvv = cvvEdit.length === 3 && /^\d{3}$/.test(cvvEdit);
+      if (!isValidCvv) {
          setCvvEdit(cvv);
          console.log('CVV should have exactly 3 numeric digits');
          return;
       }
-      const newCardData = { ...card, cvv: cvvEdit };
-      if (cvv !== cvvEdit) updateCard(newCardData).unwrap();
+      if (cvv !== cvvEdit) updateCard({ ...card, cvv: cvvEdit }).unwrap();
    };
 
-   const renderData = (label, value) => (
+   const renderDataItem = (label, value, isEditable = false) => (
       <div className={styles.data__item}>
          <div className={styles.item__data}>
             <h3 className={styles.h3}>{label}</h3>
-            <h3 className={styles.h3}>{value}</h3>
+            {isEditable ? (
+               !isCvvEdit
+                  ? <h3 className={styles.h3}>{value}</h3>
+                  : <input className={styles.input} type="number" value={cvvEdit} onChange={e => setCvvEdit(e.target.value)} maxLength={3} />)
+               : <span>{value}</span>
+            }
          </div>
-      </div>
-   );
-
-   const renderCvvData = () => (
-      <div className={styles.data__item}>
-         <div className={styles.item__data}>
-            <h3 className={styles.h3}>CVV:</h3>
-            {!isCvvEdit ? (
-               <h3 className={styles.h3}>{cvv}</h3>
-            ) : (
-               <input type="number" value={cvvEdit} onChange={e => setCvvEdit(e.target.value)} maxLength={3} />
-            )}
-         </div>
-         <EditBtn isEdit={isCvvEdit} onClick={onClickHandler} />
+         {isEditable && <EditBtn isEdit={isCvvEdit} onClick={onClickHandler} />}
       </div>
    );
 
@@ -49,12 +41,12 @@ export const CardData = ({ card }) => {
          <div className={styles.leftPart}>
             <h1 className={styles.h1}>Card Data:</h1>
             <div className={styles.data}>
-               {renderData('Number:', convertToNumberCartFormat(number))}
-               {renderData('Expiry date:', expiryDate.split('-').reverse().join(' '))}
-               {renderCvvData()}
-               {renderData('Balance:', balance)}
-               {renderData('Currency:', currency)}
-               {renderData('Type:', type)}
+               {renderDataItem('Number:', convertToNumberCartFormat(number))}
+               {renderDataItem('Expiry date:', expiryDate.split('-').reverse().join(' '))}
+               {renderDataItem('CVV:', cvv, true)}
+               {renderDataItem('Balance:', balance)}
+               {renderDataItem('Currency:', currency)}
+               {renderDataItem('Type:', type)}
             </div>
          </div>
          <div className={styles.rightPart}>
