a36474090ab29d4904ba8648071a514a64a11296
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-14 14:17:06 +0100

    update TransactionsPage

--- src/pages/TransactionsPage/TransactionsPage.jsx
+++ src/pages/TransactionsPage/TransactionsPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useGetCardsByUserIdQuery } from '../../redux';
 import { convertToNumberCartFormat } from '../../utils';
+import { Img } from '../../components'
 import styles from './TransactionsPage.module.css';
 
 export const TransactionsPage = () => {
@@ -10,18 +11,23 @@ export const TransactionsPage = () => {
 
    return (
       <div className={styles.transactionsPage}>
-         <div className='container'>
+         <div className={styles.container}>
             <ul className={styles.transactionsList}>
                {cards.map(card => (
                   <li key={card?.id} className={styles.transactionItem}>
                      <Link to={`${card.number}`} className={styles.transactionLink}>
-                        <div className={styles.transactionDetails}>
-                           <p className={styles.cardNumber}>
-                              Card Number: {convertToNumberCartFormat(card.number)}
-                           </p>
-                           <p className={styles.balance}>
-                              Balance: {card.balance} {card.currency}
-                           </p>
+                        <div className={styles.cardInfo}>
+                           <div className={styles.transactionDetails}>
+                              <p className={styles.cardNumber}>
+                                 Card Number: {convertToNumberCartFormat(card.number)}
+                              </p>
+                              <p className={styles.balance}>
+                                 Balance: {card.balance} {card.currency}
+                              </p>
+                           </div>
+                           <div className={styles.img}>
+                              <Img folder='icon/card' img={`${card.type.toLowerCase()}.png`} />
+                           </div>
                         </div>
                      </Link>
                   </li>
