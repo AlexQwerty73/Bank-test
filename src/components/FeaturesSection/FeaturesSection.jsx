adcb315f2e1cb793770b66b6b43a4090a6ab1e1b
Oleksii Tuhai <alextugai73@gmail.com>
2024-02-14 11:39:06 +0100

    update FeaturesSection

--- src/components/FeaturesSection/FeaturesSection.jsx
+++ src/components/FeaturesSection/FeaturesSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import styles from './FeaturesSection.module.css';
 import { Img } from '../commons';
+import { useInView } from 'react-intersection-observer';
 
 export const FeaturesSection = () => {
    const features = [
@@ -15,15 +16,26 @@ export const FeaturesSection = () => {
    return (
       <section className={styles.featuresSection}>
          {features.map((feature, index) => (
-            <div className={styles.feature} key={index}>
-               <div className={styles.featureContent}>
-                  <div className={styles.featureIcon}>
-                     <Img folder='featuresSection' img={feature.img} />
-                  </div>
-                  <h3 className={styles.h3}>{feature.title}</h3>
-               </div>
-            </div>
+            <FeatureItem key={index} title={feature.title} img={feature.img} />
          ))}
       </section>
+   );
+};
+
+const FeatureItem = ({ title, img }) => {
+   const [ref, inView] = useInView({
+      triggerOnce: true,
+      threshold: .7
+   });
+
+   return (
+      <div className={`${styles.feature} ${inView ? styles.visible : ''}`} ref={ref}>
+         <div className={`${styles.featureContent} `}>
+            <div className={styles.featureIcon}>
+               <Img folder='featuresSection' img={img} />
+            </div>
+            <h3 className={styles.h3}>{title}</h3>
+         </div>
+      </div>
    );
 };
